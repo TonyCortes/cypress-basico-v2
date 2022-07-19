9e78c2e966c6fd504dae578488f00062104b4267
Tony Côrtes <tonycortes.h@hotmail.com>
2022-07-19 13:07:48 -0300

    ex extra 01

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -9,10 +9,11 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it.only('Prenche os campos obrigatórios e envia os formulários', function(){
+        const longText = 'teste, teste, teste teste, teste, teste teste, teste, teste teste, teste, teste teste, teste, teste teste, teste, teste teste, teste, teste '
         cy.get('#firstName').type('Tony')
         cy.get('#lastName').type('Côrtes')
         cy.get('#email').type('tonycortes.h@test.com')
-        cy.get('#open-text-area').type('Nada não, só estou aprendendo. Obrigado!')
+        cy.get('#open-text-area').type(longText, {delay: 0})
         cy.get('button[type="submit"]').click()
 
         cy.get('.success').should('be.visible')
